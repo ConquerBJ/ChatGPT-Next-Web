ffb3e4167c2e4ccc330059a774386d21a7221839
ConquerBJ Li <pengfei.li.bjtu@gmail.com>
2023-04-18 11:12:23 +0000

    name change

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,10 +167,8 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
-          <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
-          </div>
+          <div className={styles["sidebar-title"]}>Uni Bot</div>
+          <div className={styles["sidebar-sub-title"]}>你的专属AI机器人.</div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
           </div>
